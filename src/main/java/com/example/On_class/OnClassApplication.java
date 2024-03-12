0fea6fa8978d756299c5c6b19bae3f383a7d10fc
blessed0314 <christian.daniel999@gmail.com>
2024-03-12 15:13:15 -0500

    Repositorio base del proyecto Pragma

---
+++ src/main/java/com/example/On_class/OnClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.On_class;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnClassApplication.class, args);
+	}
+
+}
