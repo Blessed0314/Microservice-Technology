5a144e1533b5bbb0ae7d2dff61319058fbb17008
blessed0314 <christian.daniel999@gmail.com>
2024-03-21 11:33:39 -0500

    Se añaden los test unitarios funcionando

--- src/test/java/com/example/On_class/adapters/driven/jpa/mysql/adapter/TechnologyAdapterTest.java
+++ src/test/java/com/example/On_class/adapters/driven/jpa/mysql/adapter/TechnologyAdapterTest.java
@@ -1,6 +1,7 @@
 package com.example.On_class.adapters.driven.jpa.mysql.adapter;
 
 import com.example.On_class.adapters.driven.jpa.mysql.entity.TechnologyEntity;
+import com.example.On_class.adapters.driven.jpa.mysql.exception.NoDataFoundException;
 import com.example.On_class.adapters.driven.jpa.mysql.mapper.ITechnologyEntityMapper;
 import com.example.On_class.adapters.driven.jpa.mysql.repository.ITechnologyRepository;
 import com.example.On_class.domain.model.Technology;
@@ -8,14 +9,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
+import java.util.Collections;
 import java.util.Optional;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 class TechnologyAdapterTest {
-
     @Mock
     private ITechnologyRepository technologyRepository;
     @Mock
@@ -42,4 +46,19 @@ class TechnologyAdapterTest {
         verify(technologyRepository).save(technologyEntity);
     }
 
+    @Test
+    void getAllTechnologies_Error_NoDataFound() {
+        int page = 0;
+        int size = 20;
+        boolean ascendingFlag = true;
+
+        PageRequest pageable = PageRequest.of(page, size, Sort.by(ascendingFlag ? Sort.Direction.ASC : Sort.Direction.DESC, "name"));
+
+        when(technologyRepository.findAll(pageable)).thenReturn(new PageImpl<>(Collections.emptyList()));
+
+        assertThrows(NoDataFoundException.class, () -> technologyAdapter.getAllTechnologies(page, size, ascendingFlag));
+
+        verify(technologyRepository).findAll(pageable);
+        verifyNoInteractions(technologyEntityMapper);
+    }
 }
